6c21148ec262e0aa6dab3b7dac5fd59fd0294fae
Caball009 <82909616+Caball009@users.noreply.github.com>
2026-03-21 13:36:32 +0100

    Tweaked debug assertion.

--- GeneralsMD/Code/GameEngine/Source/GameLogic/System/GameLogic.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/System/GameLogic.cpp
@@ -1267,7 +1267,7 @@ void GameLogic::startNewGame( Bool loadingSaveGame )
 			if (!loadingSaveGame) {
 				if (!slot->saveOffOriginalInfo())
 				{
-					DEBUG_ASSERTCRASH(m_gameMode == GAME_SKIRMISH, ("Expected skirmish game mode but got %d", m_gameMode));
+					DEBUG_ASSERTCRASH(m_gameMode == GAME_SKIRMISH, ("Expected GAME_SKIRMISH but got %s", gameModeToString(m_gameMode)));
 
 					// TheSuperHackers @fix Caball009 19/03/2026 Random color, position and faction are based on the logical seed. For improved determinism, 
 					// restarted games now set the original values so that the games start with the exact same logical seed values as the first time.
