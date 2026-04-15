6213687191ced47a56b13267190739723e014b8b
Caball009 <82909616+Caball009@users.noreply.github.com>
2026-04-15 20:22:00 +0200

    Removed duplicate virtual functions.

--- Core/GameEngine/Include/Common/ArchiveFileSystem.h
+++ Core/GameEngine/Include/Common/ArchiveFileSystem.h
@@ -126,9 +126,6 @@ public:
 	ArchiveFileSystem();
 	virtual ~ArchiveFileSystem() override;
 
-	virtual void init() = 0;
-	virtual void update() = 0;
-	virtual void reset() = 0;
 	virtual void postProcessLoad() = 0;
 
 	// ArchiveFile operations
