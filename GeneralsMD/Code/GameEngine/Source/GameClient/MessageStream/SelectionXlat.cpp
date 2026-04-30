445319173cb03ec4b791844e94d3aa91e8cb9187
Caball009 <82909616+Caball009@users.noreply.github.com>
2026-05-01 01:16:24 +0200

    Removed TSH comment.

--- GeneralsMD/Code/GameEngine/Source/GameClient/MessageStream/SelectionXlat.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/MessageStream/SelectionXlat.cpp
@@ -738,8 +738,6 @@ GameMessageDisposition SelectionTranslator::translateGameMessage(const GameMessa
 			{
 				if (!addToGroup)
 				{
-					// TheSuperHackers @tweak Update the game client but not the game logic.
-					// It isn't required for the latter, as a new group will override the current selection.
 					TheInGameUI->deselectAllDrawables(FALSE);
 				}
 
